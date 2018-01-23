516fb34cf12011f48b52101524dcfcfffed34261
York Wang <yw544@nyu.edu>
2018-01-23 04:36:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,6 +21,16 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |season, contestant|
+    contestant.each do |stats_of_contestant|
+      if stats_of_contestant["hometown"]==hometown
+        count+=1
+      end
+    end
+  end
+  count
+
 end
 
 def get_occupation(data, hometown)
