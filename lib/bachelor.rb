6b42099165e7dd7e10c5c8f8fd83cee12da7237e
York Wang <yw544@nyu.edu>
2018-01-23 04:44:02 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |stats_of_contestant|
+    if stats_of_contestant["status"].downcase =="winner"
+      return stats_of_contestant["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+data.each do |season, contestant|
+  contestant.each do |stats_of_contestant|
+    if stats_of_contestant["occupation"]==occupation
+      return stats_of_contestant["name"]
+    end
+  end
+end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |season, contestant|
+    contestant.each do |stats_of_contestant|
+      if stats_of_contestant["hometown"]==hometown
+        count+=1
+      end
+    end
+  end
+  count
+
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |data, contestant|
+    contestant.each do |stats_of_contestant|
+      if stats_of_contestant["hometown"]==hometown
+        return stats_of_contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+ages = []
+current_season = data[season]
+current_season.each do |contestant|
+  ages << contestant["age"]
+end
+age_average = ages.size
+total = 0
+ages.each do |age|
+  age = age.to_f
+  total += age
+  total
+end
+final_num = total/age_average
+final_num.round(0)
 end
